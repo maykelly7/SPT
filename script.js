69f405678d41c06b43659922afc65bdb1c41bf29
Maykelly Castro <cezariomaykelly@gmail.com>
2026-02-26 13:51:42 -0400

    s

--- script.js
+++ script.js
@@ -174,3 +174,30 @@ function createBubble() {
 
 // cria bolinhas a cada 300ms
 setInterval(createBubble, 300);
+
+const counters = document.querySelectorAll('.stat-number');
+
+counters.forEach(counter => {
+    const updateCount = () => {
+        const target = +counter.getAttribute('data-target');
+        const count = +counter.innerText;
+        const increment = target / 100;
+
+        if(count < target) {
+            counter.innerText = Math.ceil(count + increment);
+            setTimeout(updateCount, 20);
+        } else {
+            counter.innerText = target;
+        }
+    }
+
+    // opcional: só começar quando estiver visível
+    const observer = new IntersectionObserver(entries => {
+        if(entries[0].isIntersecting){
+            updateCount();
+            observer.disconnect();
+        }
+    }, {threshold: 0.5});
+
+    observer.observe(counter);
+});
